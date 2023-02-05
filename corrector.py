6a490f2dd980e3e9ddb28fe753f0debc24db266a
vedamhegde <vedamhegde@vt.edu>
2023-02-04 21:16:33 -0500

    mangoDB! connected

--- corrector.py
+++ corrector.py
@@ -1,12 +1,14 @@
 import os
 import openai
+import pymongo
+from pymongo import MongoClient
 
 # Load your API key from an environment variable or secret management service
 openai.api_key = "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"
 
 # os.getenv("OPENAI_API_KEY")
 
-email_unconf = "Dear Bob, I am really sorry to email you so last minute like this. I hope this email finds you well. I am writing to request a meeting to discuss quantum mechanics. I and very unsure about this project and believe that your expertise would be valuable to me and would help me better understand quantum mechanics. I understand that your time is valuable, and I am am sorry to bring this up so last minute. I would appreciate any time you could spare for a meeting, either in-person or via video call. Thank you so much for considering my request. I look forward to discussing quantum mechanics with you. Best regards, Veda"
+email_unconf = "Dear Bob, I am really sorry to email you so last minute like this. I hope this email finds you well. I am writing to request a meeting to discuss quantum mechanics. I am very unsure about this project and believe that your expertise would be valuable to me and would help me better understand quantum mechanics. I feel that your time is valuable, and I am sorry to bring this up so last minute. I would appreciate any time you could spare for a meeting, either in-person or via video call. Thank you so much for considering my request. I look forward to discussing quantum mechanics with you. Best regards, Veda"
 email_conf = "Dear Bob, I hope this email finds you well. I am writing to request a meeting to discuss quantum mechanics. I believe that your expertise would be valuable to me and would help me better understand quantum mechanics. I understand that your time is valuable, and I would appreciate any time you could spare for a meeting, either in-person or via video call. Please let me know your availability and I will arrange accordingly. Thank you for considering my request. I look forward to discussing quantum mechanics with you. Best regards, Veda"
 
 
@@ -40,4 +42,18 @@ def rewrite_email(email):
     print(response["choices"][0]["text"])
 
 
-rewrite_email(email_unconf) 
+def findMatches(email): 
+    #connect database
+    CONNECTION_STRING = "mongodb+srv://confidently:hackviolet23@cluster1.vdxev7c.mongodb.net/test"
+    client = MongoClient(CONNECTION_STRING)
+    db = client['PhraseData']
+    col = db.Phrases
+    words = [elem["word"] for elem in list(col.find())]
+
+    found_words = [word for word in words if word in email]
+    print(found_words)
+    
+
+
+findMatches(email_unconf)
+#rewrite_email(email_unconf) 
