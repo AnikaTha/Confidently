29c6939f7dc6d5da5b0fd8b8c2b0f6d667cde461
shriyac <shriyac@vt.edu>
2023-02-04 21:32:05 -0500

    more front end stuff

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/hello")
+def hello():
+    return "secret message!!! :O"
+
+#if you want to run it locally through command window w/ python3 app.py
+if __name__ == '__main__':
+    app.run()
+
+
